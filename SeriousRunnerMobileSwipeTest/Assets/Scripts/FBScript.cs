e2675cd00fb4cf97cb79d65a8725ac899672f8b2
ATimmeh33 <AnthonyTimmers@hotmail.com>
2016-06-01 18:09:38 +0200

    Facebook & Mixpanel Integration
    Basic integration of FB & Mixpanel. Not finished yet

---
+++ SeriousRunnerMobileSwipeTest/Assets/Scripts/FBScript.cs
@@ -0,0 +1,87 @@
+﻿using UnityEngine;
+using System.Collections;
+using Facebook.Unity;
+using System;
+using System.Collections.Generic;
+
+public class FBScript : MonoBehaviour {
+
+    public string mixpanelToken;
+
+	void Awake()
+    {
+        FB.Init(SetInit, OnHideUnity);
+
+        Mixpanel.Token = mixpanelToken;
+        Mixpanel.SuperProperties.Add("platform", Application.platform.ToString());
+        Mixpanel.SuperProperties.Add("quality", QualitySettings.names[QualitySettings.GetQualityLevel()]);
+        Mixpanel.SuperProperties.Add("fullscreen", Screen.fullScreen);
+        Mixpanel.SuperProperties.Add("resolution", Screen.width + "x" + Screen.height);
+    }
+
+    private void SetInit()
+    {
+        if (FB.IsLoggedIn)
+        {
+            Debug.Log("FB is logged in");
+        }
+        else
+        {
+            Debug.Log("FB is not logged in");
+        }
+    }
+
+    private void OnHideUnity(bool isUnityShown)
+    {
+        if (!isUnityShown)
+        {
+            Time.timeScale = 0;
+        }
+        else
+        {
+            Time.timeScale = 1;
+        }
+    }
+
+    public void FBLogIn()
+    {
+        IEnumerable<string> permissions = new string[] { "public_profile" };
+        FB.LogInWithReadPermissions(permissions, AuthCallBack);
+    }
+
+    private void AuthCallBack(ILoginResult result)
+    {
+        if (result.Error != null)
+        {
+            Debug.Log(result.Error);
+        }
+        else
+        {
+            if (FB.IsLoggedIn)
+            {
+                Debug.Log("FB is logged in");
+            }
+            else
+            {
+                Debug.Log("FB is not logged in");
+            }
+
+            FB.API("me?fields=first_name,last_name", HttpMethod.GET, RetrieveName);
+        }
+    }
+
+    private void RetrieveName(IGraphResult result)
+    {
+        Mixpanel.DistinctID = (string)result.ResultDictionary["id"];
+        Mixpanel.SendUser(new Dictionary<string, object>
+        {
+            { "$first_name", result.ResultDictionary["first_name"] },
+            { "$last_name", result.ResultDictionary["last_name"] },
+            { "$name", String.Format("{0} {1}", result.ResultDictionary["first_name"], result.ResultDictionary["last_name"]) }
+        });
+        Mixpanel.SendEvent("Login", new Dictionary<string, object>
+        {
+            { "Date", DateTime.Now.ToString("dd/MM/yyyy HH:mm") }
+        });
+    }
+}
