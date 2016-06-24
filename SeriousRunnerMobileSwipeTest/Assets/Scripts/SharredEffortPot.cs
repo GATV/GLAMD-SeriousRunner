75fb73f74e1307de806f5e8d5457a7a32cd9a9ee
GATV <gerardusvermeulen@hotmail.com>
2016-06-24 18:37:08 +0200

    Cleanest commit on github!!! xD!!!
    Description not needed ! :P

---
+++ SeriousRunnerMobileSwipeTest/Assets/Scripts/SharredEffortPot.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System;
+
+public class SharredEffortPot : MonoBehaviour {
+
+    private Guid activeSEP = new Guid("A773A316-821C-44A9-B0CD-7BFAEA17E556");
+    public Text SEPCountText;
+
+    public void SEP()
+    {
+        int sep = APIController.GetSEP(activeSEP);
+
+        SEPCountText.text = sep.ToString();
+        Debug.Log("SEP: "  + sep);
+    }
+}
